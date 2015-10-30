09dc220335d987f3a9c76564ad50bd3be0b328d0
Ghost1227 <dgriffiths@section214.com>
2015-10-29 21:41:47 -0500

    Holy codebase overhaul batman!

--- travis-ci.py
+++ travis-ci.py
@@ -2,116 +2,128 @@ import sublime
 import sublime_plugin
 import subprocess
 import os
-import re
-import urllib.request
 import json
+import urllib.request, urllib.error, urllib.parse
 
-class GitManager:
-	def __init__(self, view):
+class EventListener(sublime_plugin.EventListener):
+	def __init__(self):
+		global settings
 		settings = sublime.load_settings('Travis-CI.sublime-settings')
-		self.view = view
-		self.git = settings.get("git", "git")
-		self.remote = settings.get("remote", "origin")
-		self.prefix = settings.get("prefix", "Travis: ")
 
-	def run_git(self, cwd=None):
-		settings = sublime.load_settings('Travis-CI.sublime-settings')
-		plat = sublime.platform()
-		ret = None
-		if not cwd:
-			cwd = self.getcwd()
-		if cwd:
-			cmd = [self.git] + ["remote"] + ["show"] + [self.remote]
-			if plat == "windows":
-				startupinfo = subprocess.STARTUPINFO()
-				startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-				p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd, startupinfo=startupinfo)
-			else:
-				my_env = os.environ.copy()
-				my_env["PATH"] = "/usr/local/bin/:" + my_env["PATH"]
-				p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd, env=my_env)
-			p.wait()
-			stdoutdata, _ = p.communicate()
-			ret = stdoutdata.decode('utf-8')
-			return ret
-
-	def get_remote(self, remote):
-		ret = None
-		if remote is not None:
-			output = re.search("(Fetch URL)(.*)(?=.git)", remote, flags=re.MULTILINE)
-			if output is not None:
-				if output.group(0):
-					output = output.group(0).split(":")
-			if output is not None:
-				if output[2]:
-					ret = output[2]
-		return ret
-
-	def get_travis_json(self, remote):
-		ret = None
-		if remote is not None:
-			req = urllib.request.Request("https://api.travis-ci.org/repos/" + remote + ".json")
-			with urllib.request.urlopen(req) as response:
-				travis_json = json.loads(response.readall().decode('utf-8'))
-				ret = travis_json["last_build_status"]
-		return ret
-
-	def getcwd(self):
-		f = self.view.file_name()
-		cwd = None
-		if f:
-			cwd = os.path.dirname(f)
-		if not cwd:
-			window = self.view.window()
-			if window:
-				pd = window.project_data()
-				if pd:
-					cwd = pd.get("folders")[0].get("path")
-		return cwd
-
-	def status(self):
-		ret = None
-		remote = self.run_git()
-		remote = self.get_remote(remote)
-		status = self.get_travis_json(remote)
-		if status is not None:
-			if status == 0:
-				ret = 'Passing'
-			else:
-				ret = 'Failing'
-			settings = sublime.load_settings('Travis-CI.sublime-settings')
-			ret = self.prefix + ret
-		return ret
-
-class TravisStatusBarHandler(sublime_plugin.EventListener):
-	def update_status_bar(self, view):
-		sublime.set_timeout_async(lambda: self._update_status_bar(view), 1000)
-
-	def _update_status_bar(self, view):
-		if view.is_scratch() or view.settings().get('is_widget'):
-			return
-		gm = GitManager(view)
-		status = gm.status()
-		if status is not None:
-			view.set_status("(.0.travis-ci", status)
-		else:
-			view.erase_status("travis-ci")
-
-	def on_new(self, view):
+	def on_new_async(self, view):
 		self.update_status_bar(view)
 
-	def on_load(self, view):
+	def on_clone_async(self, view):
 		self.update_status_bar(view)
 
-	def on_activated(self, view):
-		self.update_status_bar(view)
-
-	def on_post_save(self, view):
+	def on_load_async(self, view):
 		self.update_status_bar(view)
 
 	def on_pre_close(self, view):
 		self.update_status_bar(view)
 
-	def on_window_command(self, window, command_name, args):
-		if command_name == "hide_panel":
-			self.update_status_bar(window.active_view())
+	def on_post_save_async(self, view):
+		self.update_status_bar(view)
+
+	def on_activated_async(self, view):
+		self.update_status_bar(view)
+
+	#def update_status_bar(self, view):
+		#sublime.set_timeout_async(lambda: self._update_status_bar(view), 1000)
+
+	def update_status_bar(self, view):
+		if view.is_scratch() or view.settings().get('is_widget'):
+			return
+
+		travis = TravisStatus(sublime.active_window())
+		status = travis.run()
+
+		if status is not None:
+			view.set_status("(.0.travis-ci", status)
+		else:
+			view.erase_status("(.0.travis-ci")
+
+
+
+class TravisStatus(sublime_plugin.WindowCommand):
+
+	def run(self):
+		return self.check()
+
+	def check(self):
+		remote = settings.get('default_remote', 'origin')
+		status = None
+
+		if settings.get('repos'):
+			self.repos = settings.get('repos', {})
+
+		local_repo = self.get_repo(remote)
+
+		if local_repo is not None:
+			if isinstance(self.repos, dict) and local_repo in self.repos:
+				if isinstance(self.repos[local_repo], dict) and 'remote' in self.repos[local_repo]:
+					repo = self.get_repo(self.repos[local_repo]['remote'])
+			else:
+				repo = local_repo
+
+			status = self.get_travis_status(repo)
+
+		if status is not None:
+			if status == 0:
+				status = settings.get('status_prefix', 'Travis: ') + settings.get('status_passing', 'Passing')
+			else:
+				status = settings.get('status_prefix', 'Travis: ') + settings.get('status_failing', 'Failing')
+
+		return status
+
+	def get_repo(self, remote):
+		file_name = self.window.active_view().file_name()
+		repo = None
+
+		if file_name is not None:
+			file_path, file_name = os.path.split(file_name)
+			os.chdir(file_path)
+
+			try:
+				matches = subprocess.check_output(['git', 'remote', 'show', remote]).strip()
+				matches = matches.decode('utf8', 'ignore').split("\n")
+
+				fetch = list(filter(lambda element: 'Fetch URL' in element, matches))
+
+				if len(fetch) > 0:
+					fetch = fetch[0].split(':')
+
+					if len(fetch) > 1:
+						fetch = fetch[-1]
+
+				if isinstance(fetch, str) and fetch.find('.git', 0, len(fetch)) != -1:
+					repo = fetch.strip('.git')
+			except:
+				if settings.get('debug_enable', False):
+					print('[Travis-CI Git Error] ' + file_path + ' is not a git repository')
+		else:
+			if settings.get('debug_enable', False):
+				print('[Travis-CI File Error] Unsaved file can not be in git repository')
+
+		return repo
+
+	def get_travis_status(self, repo):
+		status = None
+
+		try:
+			request = urllib.request.urlopen('https://api.travis-ci.org/repos/' + repo + '.json')
+
+			with request as travis_json:
+				travis_json = json.loads(travis_json.readall().decode('utf-8'))
+				status = travis_json["last_build_status"]
+		except urllib.error.HTTPError as error:
+			if settings.get('debug_enable', False):
+				print('[Travis-CI API Error] ' + str(error.code) + ': ' + error.reason)
+		except urllib.error.URLError as error:
+			if settings.get('debug_enable', False):
+				print('[Travis-CI API Error] ' + str(error.code) + ': ' + error.reason)
+		except Exception as error:
+			if settings.get('debug_enable', False):
+				print('[Travis-CI API Error] ' + error.read().decode())
+
+		return status
